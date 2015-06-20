7322ba689ebc982739350f5347dff9178774df98
Chase Ripplinger <chase.ripplinger@gmail.com>
2015-06-20 17:47:21 -0500

    Add basic routes

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -2,9 +2,9 @@ var server = require('nodebootstrap-server'),
     mongoose = require('mongoose');
 
 server.setup(function(runningApp) {
-  
+
   // runningApp.use(require('express-session')({secret: CONF.app.cookie_secret, resave: false, saveUninitialized: false}));
-  
+
   // Choose your favorite view engine(s)  
   runningApp.set('view engine', 'handlebars');
   runningApp.engine('handlebars', require('hbs').__express);
@@ -19,6 +19,31 @@ server.setup(function(runningApp) {
 
   runningApp.use('/hello', require('hello')); // attach to sub-route
   runningApp.use(require('routes')); // attach to root route
+
+  runningApp.post('/user', function(req, res) {
+    var username = req.body.user;
+    var password = req.body.password;
+
+    console.log("POST /user " + username + ":" + password);
+    res.status(200).json({status: 'ok'});
+  });
+
+  runningApp.post('/login', function(req, res) {
+    var username = req.body.user;
+    var password = req.body.password;
+
+    console.log("POST /login " + username + ":" + password);
+    res.status(200).json({status: 'ok'});
+  });
+
+  runningApp.post('/entry', function(req, res) {
+    var userid = req.body.id;
+    var url = req.body.url;
+    var location = req.body.location;
+
+    console.log("POST /entry id:" + userid + ", url:" + url + ", location:" + location);
+    res.status(200).json({status: 'ok'});
+  });
   
   // If you need websockets:
   // var socketio = require('socket.io')(runningApp.http);
